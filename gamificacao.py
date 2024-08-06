1d8df40b2501a7644351945713e14c3314aca0b7
jazzvestibular <alexandre@jazzvestibular.com.br>
2024-08-06 15:46:05 -0300

    Add files via upload

--- gamificacao.py
+++ gamificacao.py
@@ -565,6 +565,7 @@ def mostrar_gamificacao(nome, permissao, email):
 
         gamificacao3_aluno = gamificacao3[gamificacao3['Nome do aluno(a)'] == nome_selecionado].reset_index(drop = True)
 
+        st.dataframe(gamificacao3)
         #gamificacao3_medias = gamificacao3.drop(columns=['Nome do aluno(a)', 'Turma']).mean().reset_index()
         gamificacao3_medias = gamificacao3.drop(columns=['Nome do aluno(a)', 'Turma']).agg({'Pontuação_Engajamento_Plataforma': 'mean','Pontuação_Presença_Aulas': 'mean','Pontuação_Presença_Simulado': 'mean','Pontuação_Nota_Simulado': 'mean','Pontuação_Duvidas_Monitoria': 'mean'}).reset_index()
         
